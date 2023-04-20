163c51c87c24d55a29321ca2b00840025da0ad87
Rakesh Khadka <rakesh23khadka@gmail.com>
2023-04-20 21:44:26 +0545

    added few models

--- scraper/models.py
+++ scraper/models.py
@@ -8,3 +8,19 @@ class News(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+
+class Site(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.URLField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+    
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, related_name='site')
+    
+    def __str__(self):
+        return self.name
--- scraper/urls.py
+++ scraper/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.scrape),
-    path('index', views.index)
+    path('scrape', views.scrape, name='scrape'),
+    path('', views.index, name='home')
 ]
