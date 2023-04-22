c73ce6d13dc5aa7625da01f6e9f1454f286c4756
Rakesh Khadka <rakesh23khadka@gmail.com>
2023-04-22 11:05:04 +0545

    scrape kantipur

--- scraper/models.py
+++ scraper/models.py
@@ -5,8 +5,16 @@ from django.db import models
     
     
 class News(models.Model):
+    CATEGORY_CHOICES = [
+        ('sports', 'Sports'),
+        ('video', 'Video'),
+        ('national', 'National'),
+        ('world', 'World'),
+        ('entertainment', 'Entertainment'),
+    ]
     title = models.CharField(max_length=255)
     img_src = models.URLField(max_length=255)
     url = models.URLField(max_length=255)
+    category = models.CharField(max_length=255, choices=CATEGORY_CHOICES)
     def __str__(self):
         return self.title
